aa118f9ebd3d8c76472ef46f1f7cbab2b28ba9d6
Mattia Ceccarelli <mattia.ceccarelli3@studio.unibo.it>
2019-03-18 15:23:51 +0100

    added the first hour of lesson

--- data_pipeline.py
+++ data_pipeline.py
@@ -19,8 +19,42 @@ DATA:
         
         we can also have TSV = Tab Sep Val
         
+        JSON = commnly used on the web, ususally a dictionary of
+            dictionary at high degree
+            
+            csv = table
+            JSON = no idea of what it is 
+            JSON lines = as simple as csv but being able to parse
+            
+            INI file : simple configuration files
+            
+            YAML : rich and complicated, just used for configuration file
+    
+    XMl = really common, but never seen raw. Evolution of old html,
+        it work on tags inside tags and so on. Great for storing 
+        hyerarchical data, but is very very verbose, it takes a lot of
+        space, not easy to read.
+    
+    a lot of text format are actually XML under the hood.
+    
+        
         
     binary data : lower level
+   
+    csv and json are the easier to read.
+"""
+
+"""
+Binary Formats.
+
+the format of a binary file is not readeble by a human of course.
+For each application of a program theres a huge number of type. You want 
+them to be as fast to read as possible.
+
+Lossless Images = contains a point for each pixel (R,G,B,int) 
+    there'r no identy of object in a png format, every
     
-    
+DICOM :medical format 
+
+stopped taking notes.
 """
