efc6bd2955fdb019d281b82b5a3a73664d6382f3
Mattia Ceccarelli <mattia.ceccarelli3@studio.unibo.it>
2019-03-18 14:23:21 +0100

    created new file about pipeline and snakeman

---
+++ data_pipeline.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 14:17:51 2019
+
+@author: mattia.ceccarelli3
+"""
+
+#%%
+
+"""
+DATA:
+    
+    text data : just text, data stored as text file that needs to be read
+        by a computer. the advamtages are that a human can undrstand them
+        but are expensive in term of space (redundancy).
+        
+        Comma Separated Value = CSV 
+            values are separated in columns by a comma 
+        
+        we can also have TSV = Tab Sep Val
+        
+        
+    binary data : lower level
+    
+    
+"""
