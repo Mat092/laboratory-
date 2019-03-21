9161a3198a41239d0ce7c6b1da582ebbe3d97163
Mattia Ceccarelli <mattia.ceccarelli3@gmail.com>
2019-03-21 17:55:09 +0100

    added some exercises

--- beer_consumption.py
+++ beer_consumption.py
@@ -13,11 +13,34 @@ import seaborn as sns
 nobel_url = "https://www.britannica.com/topic/Nobel-Prize-Winners-by-Year-1856946"
 beer_url = "https://en.wikipedia.org/wiki/List_of_countries_by_beer_consumption_per_capita"
 
-nobelSet = pd.read_html(nobel_url,)
-beerSet = pd.read_html(beer_url,)
+nobelSet = pd.read_html(nobel_url,)[0]
+beerSet = pd.read_html(beer_url,)[0]
 
 
 #%%
 
-physicist_nobel = nobelSet["physics"] 
+selection = nobelSet["category"]=="physics"  
 
+physics = nobelSet[selection]
+
+#%%
+
+del physics["name"]
+del physics["year"]
+del physics["achievement"]
+del physics["literary area"]
+
+#%%
+
+nobel = pd.pivot_table(physics,
+                       index = "country*",
+                       columns = "category",
+                       aggfunc = pd.Series.count,
+        
+        )
+
+#%%
+
+
+
+sns.plot
